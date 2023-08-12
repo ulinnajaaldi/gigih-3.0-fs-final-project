18227cd664389641bc08d359aadda84e66b70704
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-08-12 18:58:58 +0700

    feat: organizing component invocation paths for better structure

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import LayoutMain from "./layout/layout-main";
+import NavigationBar from "./navbar/navbar-main";
+
+import AuthHero from "./ui/auth-hero";
+
+export { LayoutMain, NavigationBar, AuthHero };
