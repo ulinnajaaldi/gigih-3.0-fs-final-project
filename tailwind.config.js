2aa6ae58639df2a60a5b96b4ff4aefa4d4be6f23
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-08-14 00:56:13 +0700

    refactor: adjusting screen size and container padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = withMT({
     extend: {},
     screens: {
       xs: "375px",
-      sm: "640px",
+      sm: "425px",
       md: "768px",
       lg: "1024px",
       xl: "1280px",
@@ -15,7 +15,7 @@ module.exports = withMT({
     container: {
       center: true,
       padding: {
-        DEFAULT: "1rem",
+        DEFAULT: "2rem",
         sm: "2rem",
         lg: "3rem",
       },
