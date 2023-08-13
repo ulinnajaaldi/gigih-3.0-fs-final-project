21012d35d38fa797da8a8d18ca739860591ccacc
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-08-14 01:01:44 +0700

    feat: adding a reusable debounce function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,3 +3,12 @@ import { toast } from "react-toastify";
 export const toastNotify = ({ type, message }) => {
   return toast[type](message);
 };
+
+export const debounce = (fn, delay = 100) => {
+  let timeoutID;
+
+  return (...args) => {
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(() => fn(...args), delay);
+  };
+};
