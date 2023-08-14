a275ca3cf057b668cc4165f7521f52f993353fd1
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-08-15 06:46:16 +0700

    feat: adding new path pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,22 @@
 import HomePage from "./home-page";
 import UserDetailsPage from "./user-details-page";
-import VideoDetailsPage from "./vidio-details-page";
+import VideoDetailsPage from "./video-details-page";
+import NotFoundPage from "./not-found-page";
 
 import LoginPage from "./login-page";
 import RegisterPage from "./register-page";
 
 //logged user
 import DashboardPage from "./logged user/dashboard-page";
+import DashboardEditPage from "./logged user/dashboard-edit-page";
 
 export {
   HomePage,
   UserDetailsPage,
   VideoDetailsPage,
+  NotFoundPage,
   LoginPage,
   RegisterPage,
   DashboardPage,
+  DashboardEditPage,
 };
