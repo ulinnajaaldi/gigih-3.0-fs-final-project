1210f9f2a485c341606602e4457fc95c459106ef
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-08-15 06:33:54 +0700

    feat: adding functions getYoutubeEmbed and convertToIDR

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -12,3 +12,18 @@ export const debounce = (fn, delay = 100) => {
     timeoutID = setTimeout(() => fn(...args), delay);
   };
 };
+
+export const getYoutubeEmbedUrl = (url) => {
+  const youtubeUrl = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  const id = youtubeUrl[2].split(/[^0-9a-z_-]/i);
+  return `https://www.youtube.com/embed/${id[0]}`;
+};
+
+export const convertToRupiah = (number) => {
+  const rupiah = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(number);
+
+  return rupiah;
+};
