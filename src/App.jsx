e942ab8fb98409245e9d427b7fd37fab22b428ae
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-08-15 06:48:54 +0700

    feat: adding new route and route protection

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import {
   HomePage,
   UserDetailsPage,
@@ -5,17 +6,30 @@ import {
   LoginPage,
   RegisterPage,
   DashboardPage,
+  DashboardEditPage,
+  NotFoundPage,
 } from "./pages";
 import { Route, Routes } from "react-router";
+import { AuthContext } from "./contexts/auth-provider";
 
 const App = () => {
+  const { data } = useContext(AuthContext);
+
   const routes = [
+    { path: "*", element: <NotFoundPage /> },
     { path: "/", element: <HomePage /> },
     { path: "/login", element: <LoginPage /> },
     { path: "/register", element: <RegisterPage /> },
     { path: "/user/:id", element: <UserDetailsPage /> },
     { path: "/video/:id", element: <VideoDetailsPage /> },
-    { path: "/dashboard", element: <DashboardPage /> },
+    {
+      path: "/dashboard",
+      element: data === null ? <NotFoundPage /> : <DashboardPage />,
+    },
+    {
+      path: "/dashboard/edit/:id",
+      element: data === null ? <NotFoundPage /> : <DashboardEditPage />,
+    },
   ];
 
   return (
