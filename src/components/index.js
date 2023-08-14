8a75a3391f4176ece2b13e07addc18f86d45a12d
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-08-15 06:46:09 +0700

    feat: adding new path components

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,13 @@ import NavigationBar from "./navbar/navbar-main";
 
 import AuthHero from "./ui/auth-hero";
 import SpeedDialDashboard from "./ui/speed-dial-dashboard";
+import ScrollToTopButton from "./ui/scroll-to-top-button";
+import CommentList from "./ui/comment-list";
 
 import CardVideos from "./card/card-videos";
 import CardVideosUser from "./card/card-videos-user";
+import CardProducts from "./card/card-products";
+import CardProductsUser from "./card/card-products-user";
 
 import ModalDelete from "./modal/modal-delete";
 import ModalEditProfile from "./modal/modal-edit-profile";
@@ -14,18 +18,28 @@ import ModalAddProduct from "./modal/modal-add-product";
 
 import SkeletonCardVideos from "./skeleteon/skeleton-card-videos";
 import SkeletonDashboardPage from "./skeleteon/skeleton-dashboard-page";
+import SkeletonDashboardEditPage from "./skeleteon/skeleton-dashboard-edit-page";
+import SkeletonUserDetailsPage from "./skeleteon/skeleton-user-details-page";
+import SkeletonVideoDetailsPage from "./skeleteon/skeleton-video-details-page";
 
 export {
   LayoutMain,
   NavigationBar,
   AuthHero,
   SpeedDialDashboard,
+  ScrollToTopButton,
+  CommentList,
   CardVideos,
   CardVideosUser,
+  CardProducts,
+  CardProductsUser,
   ModalDelete,
   ModalEditProfile,
   ModalAddVideo,
   ModalAddProduct,
   SkeletonCardVideos,
   SkeletonDashboardPage,
+  SkeletonDashboardEditPage,
+  SkeletonUserDetailsPage,
+  SkeletonVideoDetailsPage,
 };
